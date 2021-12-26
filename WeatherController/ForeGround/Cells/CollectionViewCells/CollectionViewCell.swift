cec441cc85272a4ccfcee950599ed271d956f3ce
matildaz <irandom.con@mail.ru>
2021-12-26 17:29:33 +0300

    Added CollectionViewCell and xib file

--- WeatherController/ForeGround/Cells/CollectionViewCells/CollectionViewCell.swift
+++ WeatherController/ForeGround/Cells/CollectionViewCells/CollectionViewCell.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class CollectionViewCell: UICollectionReusableView {
+class CollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setTheLables()
     }
-
+    @IBOutlet weak var peopleImageView: UIImageView!
+    @IBOutlet weak var roomNameLable: UILabel!
+    
+    @IBOutlet weak var temperatureImageView: UIImageView!
+    @IBOutlet weak var temperatureLableView: UILabel!
+    
+    @IBOutlet weak var wetImageView: UIImageView!
+    @IBOutlet weak var wetLableView: UILabel!
+    
+    @IBOutlet weak var co2ImageView: UIImageView!
+    @IBOutlet weak var co2LableView: UILabel!
+    
+    func setTheLables() {
+        roomNameLable.text = "Room"
+        temperatureLableView.text = "CurrTemp"
+        wetLableView.text = "CurrWet"
+        co2LableView.text = "CurrCO2"
+    }
+    
+    
 }
--- WeatherController/ForeGround/ViewControllers/RoomsViewController.swift
+++ WeatherController/ForeGround/ViewControllers/RoomsViewController.swift
@@ -15,3 +15,16 @@ class RoomsViewController: UIViewController {
     }
     
 }
+
+extension RoomsViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = CollectionViewCell()
+        return cell
+    }
+    
+    
+}
