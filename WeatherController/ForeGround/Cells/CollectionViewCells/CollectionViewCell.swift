bc2b470335af2311048a3489956aa68e45fa5524
matildaz <irandom.con@mail.ru>
2021-12-26 15:53:20 +0300

    First steps in designing. Added ViewControllers at main storyboard

---
+++ WeatherController/ForeGround/Cells/CollectionViewCells/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  WeatherController
+//
+//  Created by Mikhail Chibrin on 26.12.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ WeatherController/ForeGround/ViewControllers/RoomsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  WeatherController
+//
+//  Created by Mikhail Chibrin on 26.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class RoomsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
