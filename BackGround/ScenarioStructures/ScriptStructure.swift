c21d30c2519c8e0440b5cdac0bd6d2bd3fa525f8
matildaz <irandom.con@mail.ru>
2022-02-08 23:43:17 +0300

    Added layer for scriptsVC

---
+++ BackGround/ScenarioStructures/ScriptStructure.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptStructure.swift
+//  WeatherController
+//
+//  Created by Mikhail Chibrin on 26.12.2021.
+//
+
+import Foundation
+
+struct ScriptSctructure : Codable {
+    
+    var did: String?
+    var name: String?
+    var scriptDescription: String?
+    var roomGroop0: [RoomGroupStructure]?
+    
+}
